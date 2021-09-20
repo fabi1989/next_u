556af5de3995d62924f871b9e67c98c5d480b842
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-09-20 23:33:23 +0200

    numeros_primos

--- funcion.py
+++ funcion.py
@@ -1,4 +1,11 @@
 inf = int(input("limite inferior del intervalo: "))
 sup = int(input("limite superior del intervalo: "))
 print("los numeros primos entre", inf, "y", sup, "son: ")
-for num in range(inf, sup+1).count
+for num in range(inf, sup+1):
+    es_primo = True
+    i = 2
+    while (es_primo and i < num):
+        es_primo = (num % i) != 0
+        i += 1
+        if es_primo:
+            print(num, "es un numero primo")
