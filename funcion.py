af895b01f31a0e159bdb050215c19eb6e3d3edef
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-09-20 23:13:04 +0200

    saldra

--- funcion.py
+++ funcion.py
@@ -1,5 +1,4 @@
-kaixooo = "hola, en español"
-print(kaixooo)
-
-hola = "hahah"
-print(hola)
+inf = int(input("limite inferior del intervalo: "))
+sup = int(input("limite superior del intervalo: "))
+print("los numeros primos entre", inf, "y", sup, "son: ")
+for num in range(inf, sup+1)
