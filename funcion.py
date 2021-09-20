81ed7a09d09175a45294cdb34e8b90951d2b72d0
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-09-20 23:19:59 +0200

    si_salio

--- funcion.py
+++ funcion.py
@@ -1,4 +1,4 @@
 inf = int(input("limite inferior del intervalo: "))
 sup = int(input("limite superior del intervalo: "))
 print("los numeros primos entre", inf, "y", sup, "son: ")
-for num in range(inf, sup+1)
+for num in range(inf, sup+1).count
