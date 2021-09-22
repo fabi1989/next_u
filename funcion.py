3339eefcafbf2ce95f567a1d4f03758ffaa40d6e
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-09-22 15:55:35 +0200

    ingresar_moned

--- funcion.py
+++ funcion.py
@@ -1,3 +1,20 @@
-numeros = ['1', '2']
-for numero in numeros:
-    print(numero)
+_ENDPOINT = "https://api.binance.com"
+
+
+def _url(api):
+    return _ENDPOINT+api
+
+    def get_price(cripto):
+        return requests.get(_url("/api/v3/ticker/price?symbol="+cripto))
+
+    def esmoneda(cripto):
+        criptos = ["BTC", "BBC", "LTC", "ETC", "XRP"]
+        return cripto in criptos
+
+    def esnumero(numero):
+        return numero.replace('.', '', 1).isdigit()
+
+    monedas = []
+    cantidades = []
+    cotizaciones = []
+    i = 0
