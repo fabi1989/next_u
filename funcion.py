4e0e40968ae852326a481ebe69b0833c06b39158
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-09-22 15:41:32 +0200

    hacer_lista_de_numeros

--- funcion.py
+++ funcion.py
@@ -1,14 +1,3 @@
-x = int(input("ingrese primero numero: "))
-y = int(input("ingrese segundo numero: "))
-z = int(input("ingrese tercer numero: "))
-print("el maximo entre", x, ",", y, "y", z, "es", max(x, y, z), )
-
-
-def max(a, b,):
-    """ esta funcion calcula el maximo entre dos numeros"""
-
-    if a >= b:
-        maximo = a
-    else:
-        maximo = b
-        return maximo
+numeros = ['1', '2']
+for numero in numeros:
+    print(numero)
