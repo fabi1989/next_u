587f618e6c21e7dc951a15e85b206d315f77349b
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-09-29 18:53:53 +0200

    haciendo examen

--- proyecto.py
+++ proyecto.py
@@ -1,17 +1,55 @@
-def opciones(primeraopcion):
-    opcion = ["solicitar moneda","cantidad a recibir","codigo"]
-    return primeraopcion in opcion
-
-lasopciones=[]
-i=0
-
-while i <=3:
-    print("que opcion desea realizar: ")
-    opcion=input("recibir cantidad, transferir monto, mostrar balance de una moneda, mostrar balance general, mostrar historico de transacciones, salir de programa: ")
-    while not opciones(opcion):
-        print("opcion invalida.")
-        lasopciones =input("ingrese correctamente la opcion a realizar: ")
+import os
 
 
+clear = lambda: os.system('cls')
+
+monedas = [
+    'BTC',
+    'DCC',
+]
 
 
+def recibir_cantidad():
+    print('RECIBIR CANTIDAD!')
+    print('----------')
+    print('Tipos de moneda:')
+
+    for moneda in monedas:
+        print('1. ', moneda)
+
+    tipo_de_moneda = input('Ingrese su tipo de moneda: ')
+    print(tipo_de_moneda)
+
+
+def transferir_monto():
+    print('Transfiriendo monto')
+
+
+def menu(mensaje = ''):
+    clear()
+
+    if (mensaje):
+        print(mensaje)
+
+    print('MENU PRINCIPAL!')
+    print('----------')
+    print("1. Recibir Cantidad")
+    print("2. Transferir Monto")
+    print("6. Salir")
+    print('----------')
+
+    opcion = input('Ingrese su opcion: ')
+
+    if opcion == '1':
+        clear()
+        recibir_cantidad()
+    elif opcion == '2':
+        clear()
+        transferir_monto()
+    elif opcion == '6':
+        print("Gracias por nada!")
+    else:
+        menu("Esa opcion no existe, vuelva a ingresar su opcion.")
+
+
+menu()
