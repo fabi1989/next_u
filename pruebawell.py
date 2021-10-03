957d0a79b96de12b17d1b4387bfd8354527b4081
Fabian Zambrano <zambranosalcedo@gmail.com>
2021-10-03 14:30:58 +0200

    siti_web

--- pruebawell.py
+++ pruebawell.py
@@ -1,2 +1,32 @@
+# **
+# ***
+# ****
+# *****
+def prueba1():
+  contador_de_lineas = 0
+  while(contador_de_lineas < 4):
 
-  
+    contador_de_asteriscos =1
+    while(contador_de_asteriscos < contador_de_lineas):
+        print('*', end='')
+        contador_de_asteriscos = contador_de_asteriscos + 1
+
+    print(contador_de_lineas+1)
+    contador_de_lineas = contador_de_lineas + 1
+
+prueba1()
+
+### LOOP 1
+# contador_de_lineas = 
+# contador_de_asteriscos = 
+# 
+
+#######
+# 
+# 
+# 
+# 
+
+
+
+
